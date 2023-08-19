c503883d5dd66b82f3a19bf6ca91133579e08f67
sid <sid@adoltech.com>
2023-08-19 22:31:35 +0530

    Summarization code Improvements

--- PyStuff/TransferLearningStuff/Summarization/main.py
+++ PyStuff/TransferLearningStuff/Summarization/main.py
@@ -3,7 +3,8 @@ import pprint
 
 s = str(input("Enter Content: "))
 x = int(input("\nMinimal Word Count: "))
-summarizer = pipeline(task="summarization", model="facebook/bart-large-cnn")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+summarizer = pipeline(task="summarization", model="facebook/bart-large-cnn", device=device)
 ans = summarizer(s, min_length=x)
 
 pprint.pprint(ans)
