f7688c6bfe5bb154f978b26f718d9d226234cb78
sid <sid@adoltech.com>
2023-10-20 20:22:08 +0530

    RecBook Face Detection Integration

--- FlutterStuff/rec_book/lib/Pages/FaceSignUpPage.dart
+++ FlutterStuff/rec_book/lib/Pages/FaceSignUpPage.dart
@@ -1,7 +1,9 @@
-import 'dart:typed_data';
-import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+
+import 'workinFlows/detectorView.dart';
+import 'workinFlows/boxPainter.dart';
 
 class FaceSignUpPage extends StatefulWidget {
   @override
@@ -9,121 +11,66 @@ class FaceSignUpPage extends StatefulWidget {
 }
 
 class _FaceSignUpPageState extends State<FaceSignUpPage> {
-  late CameraController _controller;
-  late FaceDetector _faceDetector;
-  List<Rect> _detectedFaces = [];
+   final FaceDetector _faceDetector = FaceDetector(
+    options: FaceDetectorOptions(
+      enableContours: true,
+      enableLandmarks: true,
+    ),
+  );
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  var _cameraLensDirection = CameraLensDirection.front;
 
   @override
-  void initState() {
-    super.initState();
-
-    _controller = CameraController(
-      CameraDescription(
-        name: 'Camera',
-        lensDirection: CameraLensDirection.front,
-      ),
-      ResolutionPreset.medium,
-    );
-
-    _faceDetector = GoogleMlKit.vision.faceDetector(
-      FaceDetectorOptions(
-        enableClassification: false,
-        enableTracking: false,
-        enableContours: true,
-      ),
-    );
-
-    _controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-      _controller.startImageStream((CameraImage img) {
-        _detectFaces(img);
-      });
-    });
-  }
-
-  void _detectFaces(CameraImage image) async { 
-    final inputImage = InputImage.fromBytes( 
-      bytes: _concatenatePlanes(image.planes),
-      inputImageData: InputImageData(
-        inputImageFormat: InputImageFormatMethods.fromRawValue(image.format.raw) ?? InputImageFormat.NV21,
-        size: Size(image.width.toDouble(), image.height.toDouble()),
-        rotation: InputImageRotation.Rotation_90deg, 
-        planeData: image.planes.map((plane) {
-          return InputImagePlaneMetadata(
-            bytesPerRow: plane.bytesPerRow,
-            height: plane.height,
-            width: plane.width,
-          );
-        }).toList(),
-      ),
-    );
-
-    final faces = await _faceDetector.processImage(inputImage);
-    for (Face face in faces) {
-      print('face BoundingBox Yo: ${face.boundingBox}');
-    }
-
-    setState(() {
-      _detectedFaces = faces.map((face) => face.boundingBox).toList();
-    });
-  }
-
-  Uint8List _concatenatePlanes(List<Plane> planes) {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (Plane plane in planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-    return allBytes.done().buffer.asUint8List();
+  void dispose() {
+    _canProcess = false;
+    _faceDetector.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!_controller.value.isInitialized) {
-      return Container();
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Signing Up Face'),
-      ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: _controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              CameraPreview(_controller),
-              CustomPaint(
-                painter: BoundingBoxPainter(_detectedFaces),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return DetectorView(
+      title: 'Face Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
     );
   }
-}
 
-class BoundingBoxPainter extends CustomPainter {
-  final List<Rect> faces;
-
-  BoundingBoxPainter(this.faces);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.red
-      ..style = PaintingStyle.stroke 
-      ..strokeWidth = 2.0;
-
-    for (final rect in faces) {
-      canvas.drawRect(rect, paint);
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final faces = await _faceDetector.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = FaceDetectorPainter(
+        faces,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      String text = 'Faces found: ${faces.length}\n\n';
+      for (final face in faces) {
+        text += 'face: ${face.boundingBox}\n\n';
+      }
+      _text = text;
+      // TODO: set _customPaint to draw boundingRect on top of image
+      _customPaint = null;
     }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
--- FlutterStuff/rec_book/lib/Pages/workinFlows/detectorView.dart
+++ FlutterStuff/rec_book/lib/Pages/workinFlows/detectorView.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 
 import 'cameraView.dart';
-//import 'gallery_view.dart';
 
 enum DetectorViewMode { liveFeed, gallery }
 
@@ -46,28 +45,18 @@ class _DetectorViewState extends State<DetectorView> {
 
   @override
   Widget build(BuildContext context) {
-    return _mode == DetectorViewMode.liveFeed
-        ? CameraView(
-            customPaint: widget.customPaint,
-            onImage: widget.onImage,
-            onCameraFeedReady: widget.onCameraFeedReady,
-            onDetectorViewModeChanged: _onDetectorViewModeChanged,
-            initialCameraLensDirection: widget.initialCameraLensDirection,
-            onCameraLensDirectionChanged: widget.onCameraLensDirectionChanged,
-          )
-        : GalleryView(
-            title: widget.title,
-            text: widget.text,
-            onImage: widget.onImage,
-            onDetectorViewModeChanged: _onDetectorViewModeChanged);
+    return CameraView(
+      customPaint: widget.customPaint,
+      onImage: widget.onImage,
+      onCameraFeedReady: widget.onCameraFeedReady,
+      onDetectorViewModeChanged: _onDetectorViewModeChanged,
+      initialCameraLensDirection: widget.initialCameraLensDirection,
+      onCameraLensDirectionChanged: widget.onCameraLensDirectionChanged,
+    );
   }
 
   void _onDetectorViewModeChanged() {
-    if (_mode == DetectorViewMode.liveFeed) {
-      _mode = DetectorViewMode.gallery;
-    } else {
-      _mode = DetectorViewMode.liveFeed;
-    }
+    _mode = DetectorViewMode.liveFeed;
     if (widget.onDetectorViewModeChanged != null) {
       widget.onDetectorViewModeChanged!(_mode);
     }
